37cca50e67c7a2be43b9977aa5d69c5cd278c99b
Fetch <59113947+fetchhh@users.noreply.github.com>
2024-07-29 10:28:25 +0200

    Update sharp-config.js

--- middlewares/sharp-config.js
+++ middlewares/sharp-config.js
@@ -22,7 +22,7 @@ const compress = (request, response, next) => {
         // Delete previous file
         fs.unlink(filePath, (err) => {
           if (err) {
-            //console.log(err);
+            console.log(err);
           }
         });
         next();
